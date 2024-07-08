611955f8104f8dad080e25e361b441bb024b5f92
GTG-Consultoria <contato@gtgconsultoria.com.br>
2024-07-08 02:08:03 +0000

    Priemira alteração

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,11 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+fruta_favorita = 'maça'
 # Declare variáveis de cada tipo numérico
+numero = 6
+salário = 1.200,00
 
-
+complexo = 0+2j 
 # Declare uma variável do tipo booleano
+teste = False
