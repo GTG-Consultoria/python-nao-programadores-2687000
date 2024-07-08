12a422b362e25548aedf73cd12d2f85fd9d437b7
GTG-Consultoria <contato@gtgconsultoria.com.br>
2024-07-08 02:26:28 +0000

    Primeira alteração

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,28 +1,35 @@
 resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
 
 # Imprima na tela a variável "resumo"
-
+print(resumo)
 
 # Imprima na tela apenas a segunda letra da variável
-
+print(resumo[1])
 
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
-
+idade =46
+print(idade)
+#ou
+print(resumo[23:25])
 
 # Imprima na tela o trecho final da variável
-
+palavra = split(resumo)
+ultima_palavra = paralvra(-1)
+print(ultima_palavra)
+#ou
+print(resumo[62:])
 
 # Converta todos as letras para minúsculo e imprima na tela
-
+print(resumo.lower())
 
 # Converta todas as letras para maiúscula e imprima na tela
-
+print(resumo.upper())
 
 # Formate a frase para que a primeira letra de cada palavra seja maiúscula e imprima na tela
-
+print(resumo.title())
 
 # Formate a frase para que apenas a primeira letra da frase seja maiúscula e imprima na tela
-
+print(resumo.capitalize())
 
 # Imprima na tela uma string utilizando uma variável, usando o recurso string format
-
+print(f'A idade dela é: {idade} anos') 
